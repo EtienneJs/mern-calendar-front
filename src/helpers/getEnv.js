f14cd22a52312e2c6e9b03789d69da60b6b6ae22
eter19 <claudita.et@gmail.com>
2023-01-18 19:27:22 -0300

    change getEnv

--- src/helpers/getEnv.js
+++ src/helpers/getEnv.js
@@ -2,6 +2,6 @@ export const getEnv = () => {
   import.meta.env.VITE_API_URL;
   console.log(import.meta.env.VITE_API_URL);
   return {
-    ...import.meta.env,
+    ...import.meta.env.VITE_API_URL,
   };
 };
